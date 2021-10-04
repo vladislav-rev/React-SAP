ad46310a0ff2260716a1c6c4dd647cc8f09ad1ad
Vlad Rev <vergigo89@gmail.com>
2021-10-04 23:28:44 +0500

    deploy test-task-galago

---
+++ my-app/build/precache-manifest.12886163a7efd3ac5d667424ec97653e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43958b39258c9eb4b579a25a2bd6701c",
+    "url": "/React-SAP/index.html"
+  },
+  {
+    "revision": "c38ff8b2ce464714b4e7",
+    "url": "/React-SAP/static/css/main.afc234d6.chunk.css"
+  },
+  {
+    "revision": "79c1de244415ea911d11",
+    "url": "/React-SAP/static/js/2.fb115b5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-SAP/static/js/2.fb115b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38ff8b2ce464714b4e7",
+    "url": "/React-SAP/static/js/main.dbdac756.chunk.js"
+  },
+  {
+    "revision": "fc3b8bd90fb8cd9697a1",
+    "url": "/React-SAP/static/js/runtime-main.765978c5.js"
+  }
+]);
